505660846e046ab5890d1450b918b8fe9317bd46
Chuizheng Meng <chuizhem@usc.edu>
2021-10-18 10:00:15 -0700

    Update install.sh
    added version requirement of pytorch-lightning

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 conda install -y numpy scipy pandas jupyter matplotlib pyyaml scikit-learn tqdm networkx jupyterlab
 conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch
-pip install pytorch-lightning
+pip install pytorch-lightning==0.9.0
 pip install wandb
 pip install torch-scatter==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
